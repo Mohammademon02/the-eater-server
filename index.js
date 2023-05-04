1dae968b5e9de59731fd376cf527b506969ba2da
mohammademon02 <mohammademon0207@gmail.com>
2023-05-04 15:36:03 +0600

    chef file load and send success

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chef = require('./data/chef.json');
+
 
 app.get('/', (req, res) =>{
     res.send('The Eater is running')
+})
+
+app.get('/chef', (req, res) =>{
+    res.send(chef)
 })
 
 app.listen(port, () => {
