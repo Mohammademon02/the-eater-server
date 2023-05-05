e60dd1cb7cee5fd5f22ca8d19ae89076a07d31a7
mohammademon02 <mohammademon0207@gmail.com>
2023-05-05 21:46:07 +0600

    vercel config setup

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const chef = require('./data/chef.json');
 
+app.use(cors());
 
 app.get('/', (req, res) =>{
     res.send('The Eater is running')
@@ -12,6 +14,13 @@ app.get('/', (req, res) =>{
 app.get('/chef', (req, res) =>{
     res.send(chef)
 })
+
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chef.find( n=> n.id == id);
+    res.send(selectedChef)
+    
+} )
 
 app.listen(port, () => {
     console.log(`The Eater is running on port: ${port}`)
